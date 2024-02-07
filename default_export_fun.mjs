828fa17641e4a89bc966ae27fa813479da0246cc
SIDDHARTH-101251 <singhsiddharth101251@gmail.com>
2024-02-07 23:39:37 +0530

    Added default_export_fun.mjs

---
+++ default_export_fun.mjs
@@ -0,0 +1,9 @@
+//we can export function while defining as we as after defining
+
+export function sum(num1, num2) {
+    return num1 + num2;
+}
+
+export function sub(num1, num2) {
+    return num1 - num2;
+}
--- index.mjs
+++ index.mjs
@@ -1,3 +1,10 @@
 import x from "./default_export_var.mjs"
+import res from "./default_export_expr.mjs"
+import {sum, sub} from "./default_export_fun.mjs"
+
+
 
 console.log(x);
+console.log(res);
+console.log(sum(10, 20));
+console.log(sub(20, 10));
