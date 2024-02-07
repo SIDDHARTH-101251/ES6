d3e0364d3207352aa9cc486909d1668c4cd45408
SIDDHARTH-101251 <singhsiddharth101251@gmail.com>
2024-02-07 23:48:19 +0530

    Added file named_export_var.mjs

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import x from "./default_export_var.mjs"
 import res from "./default_export_expr.mjs"
 import sum from "./default_export_fun.mjs"
 import Car from "./default_export_class.mjs"
-
+import {v, w} from "./named_export_var.mjs"
 
 console.log(x);
 console.log(res);
@@ -10,3 +10,5 @@ console.log(sum(10, 20));
 
 const CarDetails = new Car("Audi");
 console.log(CarDetails);
+
+console.log(v," ", w)
---
+++ named_export_var.mjs
@@ -0,0 +1,4 @@
+//we can export variable while defining as well as fater defining
+
+export var v = 10;
+export var w = 20;
