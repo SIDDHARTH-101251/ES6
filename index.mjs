12ce0a0eb676de1676ec0017b928837c61439302
SIDDHARTH-101251 <singhsiddharth101251@gmail.com>
2024-02-07 23:52:17 +0530

    Added named_export_fun.mjs

--- index.mjs
+++ index.mjs
@@ -3,6 +3,7 @@ import res from "./default_export_expr.mjs"
 import sum from "./default_export_fun.mjs"
 import Car from "./default_export_class.mjs"
 import {v, w} from "./named_export_var.mjs"
+import {mult, div} from "./named_export_fun.mjs"
 
 console.log(x);
 console.log(res);
@@ -12,3 +13,5 @@ const CarDetails = new Car("Audi");
 console.log(CarDetails);
 
 console.log(v," ", w)
+console.log(div(10, 20));
+console.log(mult(20, 10));
---
+++ named_export_fun.mjs
@@ -0,0 +1,7 @@
+export function div(num1, num2) {
+    return num1 / num2;
+}
+
+export function mult(num1, num2) {
+    return num1 * num2;
+}
